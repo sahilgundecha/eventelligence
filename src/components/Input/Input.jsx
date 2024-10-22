c89e60d55be3bf802201c8a7b3462b9701288269
Sahil Gundecha <gundechasahil@gmail.com>
2024-10-22 21:36:48 +0530

    Add checkbox component, fix login screen issue

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,32 +1,31 @@
-import React from 'react';
+import React from "react";
 
 const Input = (props) => {
   const {
-    type = 'text',
+    type = "text",
     id,
-    name = '',
+    name = "",
     value,
     placeholder,
     onChange,
     error,
     required = false,
-    className = '',
-    label,
-    labelClasses = '',
-    inputClasses = '',
+    className = "",
+    label = "",
+    labelClasses = "",
+    inputClasses = "",
     register,
-    registerOptions,
     ...rest
   } = props;
 
   return (
-    <div className='input-wrapper relative mb-2 w-full'>
+    <div className="input-wrapper relative mb-2 w-full">
       <label
         htmlFor={id}
-        className={'block text-gray-700 font-bold mb-2 text-xs' + labelClasses}
+        className={"block text-gray-700 font-bold mb-2 text-xs" + labelClasses}
       >
         {label}
-        {required && <span className='text-red-500'>*</span>}
+        {required && <span className="text-red-500">*</span>}
       </label>
       <input
         type={type}
@@ -37,15 +36,14 @@ const Input = (props) => {
         placeholder={placeholder}
         className={`w-full px-3 py-2 border rounded-lg ${
           error
-            ? 'border-red-500 focus:outline-red-500 focus:ring-red-500'
-            : 'border-gray-300 focus:outline-[#201502] focus:ring-[#201502]'
+            ? "border-red-500 focus:outline-red-500 focus:ring-red-500"
+            : "border-gray-300 focus:outline-[#201502] focus:ring-[#201502]"
         } ${className} ${inputClasses}`}
         required={required}
         {...register}
-        // {...register('name')}
-        // {...rest}
+        {...rest}
       />
-      {<p className='text-red-500 text-xs absolute'>{error && error}</p>}
+      {<p className="text-red-500 text-xs absolute">{error && error}</p>}
     </div>
   );
 };
--- src/screens/LoginPage/Login.jsx
+++ src/screens/LoginPage/Login.jsx
@@ -1,91 +1,160 @@
 // src/screens/Login.jsx
-import React from 'react';
-import Logo from '../../assets/images/logo.png';
-import LoginBG from '../../assets/images/login.svg';
-import {
-  Checkbox,
-  TextField,
-  Button,
-  Typography,
-  Link,
-  Box,
-} from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import Logo from "../../assets/images/logo.png";
+import LoginBG from "../../assets/images/login.svg";
+import { useNavigate } from "react-router-dom";
+import Checkbox from "../../components/Checkbox/Checkbox";
 
 const Login = () => {
   const navigate = useNavigate();
   return (
-    <div className='flex h-screen overflow-hidden'>
-      {/* Left section with form */}
+    // <div className="flex h-screen">
+    //   {/* Left section with form */}
 
-      <div className='w-2/5 bg-[#F3F3F3] flex flex-col justify-center text-left px-16 items-center'>
-        <div className='max-width'>
-          <div className='flex items-center mb-4'>
-            <img src={Logo} alt='EventNest Logo' className='w-auto h-auto' />
+    //   <div className="w-2/5 bg-[#F3F3F3] flex flex-col justify-center text-left px-16 items-center">
+    //     <div className="max-width">
+    //       <div className="flex items-center">
+    //         <img src={Logo} alt="EventNest Logo" />
+    //       </div>
+
+    //       <h3 className="text-xl font-bold mb-2">Welcome to EventNest!</h3>
+    //       <p className="mb-6 text-[#7b746a]">
+    //         Please enter your Login ID & password to continue
+    //       </p>
+
+    //       {/* Login Form */}
+    //       <div className="flex flex-col mb-2 gap-2">
+    //         <label htmlFor="loginId" className="block text-gray-700">
+    //           Login ID
+    //         </label>
+    //         <input
+    //           autoFocus
+    //           type="text"
+    //           id="loginId"
+    //           placeholder="Login ID"
+    //           className="border rounded-md px-3 py-2 focus:outline-none focus:border-[#FF5B2E]"
+    //         />
+
+    //         <label htmlFor="password" className="block text-gray-700">
+    //           Password
+    //         </label>
+    //         <input
+    //           type="password"
+    //           id="password"
+    //           placeholder="Password"
+    //           className="border rounded-md px-3 py-2 focus:outline-none focus:border-[#FF5B2E]"
+    //         />
+    //       </div>
+
+    //       {/* Remember me and Reset Password */}
+    //       <div className="flex items-center justify-between mb-6">
+    //         <div className="flex items-center">
+    //           <input
+    //             type="checkbox"
+    //             className="mr-2 border rounded-md checked:bg-[#FF5B2E] checked:border-[#FF5B2E]"
+    //           />
+    //           <label className="font-[#000000] text-sm font-semibold">
+    //             Remember me
+    //           </label>
+    //         </div>
+    //         <a href="#" className="text-[#FF5B2E] text-sm font-bold">
+    //           Reset password
+    //         </a>
+    //       </div>
+
+    //       {/* Login Button */}
+    //       <button
+    //         className="bg-[#201502] text-white px-4 py-2 rounded-md w-full"
+    //         type="submit"
+    //         onClick={() => navigate("/dashboard")}
+    //       >
+    //         Login
+    //       </button>
+    //     </div>
+    //   </div>
+    //   {/* Right section with image */}
+    //   <div className="w-3/5">
+    //     <img
+    //       src={LoginBG}
+    //       alt="Event"
+    //       className="w-full object-cover h-full"
+    //       fetchPriority="high"
+    //     />
+    //   </div>
+    // </div>
+
+    <div className="flex flex-col md:flex-row h-screen">
+      {/* Left section with form */}
+      <div className="w-full md:w-2/5 bg-[#F3F3F3] flex flex-col justify-center text-left px-6 md:px-16 items-center h-screen">
+        <div className="max-w-md w-full">
+          <div className="flex items-center justify-center md:justify-start mb-4">
+            <img src={Logo} alt="EventNest Logo" className="h-10 md:h-auto" />
           </div>
 
-          <h3 className='text-xl font-bold mb-2'>Welcome to EventNest!</h3>
-          <p className='mb-6 text-[#7b746a]'>
+          <h3 className="text-lg md:text-xl font-bold mb-2">
+            Welcome to EventNest!
+          </h3>
+          <p className="mb-6 text-[#7b746a] text-sm md:text-base text-center md:text-left">
             Please enter your Login ID & password to continue
           </p>
 
           {/* Login Form */}
-          <div className='flex flex-col mb-2 gap-2'>
-            <label htmlFor='loginId' className='block text-gray-700'>
+          <div className="flex flex-col mb-4 gap-2">
+            <label htmlFor="loginId" className="block text-gray-700">
               Login ID
             </label>
             <input
               autoFocus
-              type='text'
-              id='loginId'
-              placeholder='Login ID'
-              className='border rounded-md px-3 py-2 focus:outline-none focus:border-[#FF5B2E]'
+              type="text"
+              id="loginId"
+              placeholder="Login ID"
+              className="border rounded-md px-3 py-2 focus:outline-none focus:border-[#FF5B2E] w-full"
             />
 
-            <label htmlFor='password' className='block text-gray-700'>
+            <label htmlFor="password" className="block text-gray-700">
               Password
             </label>
             <input
-              type='password'
-              id='password'
-              placeholder='Password'
-              className='border rounded-md px-3 py-2 focus:outline-none focus:border-[#FF5B2E]'
+              type="password"
+              id="password"
+              placeholder="Password"
+              className="border rounded-md px-3 py-2 focus:outline-none focus:border-[#FF5B2E] w-full"
             />
           </div>
 
           {/* Remember me and Reset Password */}
-          <div className='flex items-center justify-between mb-6'>
-            <div className='flex items-center'>
-              <input
-                type='checkbox'
-                className='mr-2 border rounded-md checked:bg-[#FF5B2E] checked:border-[#FF5B2E]'
-              />
-              <label className='font-[#000000] text-sm font-semibold'>
-                Remember me
-              </label>
-            </div>
-            <a href='#' className='text-[#FF5B2E] text-sm font-bold'>
+          <div className="flex flex-col lg:items-center justify-between mb-6 lg:flex-row">
+            <Checkbox
+              label="Rememer me"
+              id="rememberMe"
+              inputClasses="w-4 h-4"
+            />
+            <a
+              href="#"
+              className="text-[#FF5B2E] text-sm font-bold hover:underline"
+            >
               Reset password
             </a>
           </div>
 
           {/* Login Button */}
           <button
-            className='bg-[#201502] text-white px-4 py-2 rounded-md w-full'
-            type='submit'
-            onClick={() => navigate('/dashboard')}
+            className="bg-[#201502] text-white px-4 py-2 rounded-md w-full"
+            type="submit"
+            onClick={() => navigate("/dashboard")}
           >
             Login
           </button>
         </div>
       </div>
+
       {/* Right section with image */}
-      <div className='w-3/5'>
+      <div className="hidden md:block md:w-3/5">
         <img
           src={LoginBG}
-          alt='Event'
-          className='w-full object-cover h-auto'
-          fetchPriority='high'
+          alt="Event"
+          className="w-full object-cover h-full"
+          fetchPriority="high"
         />
       </div>
     </div>
